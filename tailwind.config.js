6f601feba47a33e9e3f6988de22101cd9046d81e
danishyaqoobofficial <danishyaqoob117@gmail.com>
2024-08-15 13:07:33 +0500

    completed landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}" , "index.html"],
   theme: {
+    colors: {
+      primary: '#E50512',
+      theme_black: '#202020',
+      grey1: '#797979',
+      grey2: '#D2D2D2',
+      theme_white: '#FFFFFF',
+    },
     extend: {},
   },
   plugins: [],
